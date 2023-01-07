38b0f4914308919aeda9012ae95627cf4d1f594e
Antonio Jose Barrios Leon <antoniojose.barrios@securitasdirect.es>
2023-01-07 13:45:08 +0100

    feat: add server main file

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const app = express()
+
+const DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost:27017/game-of-thrones'
+const SERVER_PORT = process.env.SERVER_PORT || 3000 
+
+mongoose.set('strictQuery', true)
+mongoose
+  .connect(DATABASE_URL)
+  .then(console.log('Connected to DB'))
+  .catch((err) => console.log('Error in connection to DB =>', err.message))
+
+app.use(express.json())
+
+const charactersRouter = require('./src/routes/characters')
+app.use('/characters', charactersRouter)
+
+app.listen(SERVER_PORT, () => {
+  console.log(`Server running at port ${SERVER_PORT}`)
+})
